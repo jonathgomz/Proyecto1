56e6dc643cbc2b8c3e11dcaa283182a7785a02b6
Jonathan <Jonathan@DESKTOP-UEVN9UU>
2025-11-22 23:17:55 -0500

    ahora si Calculadora api

---
+++ CalculadoraAPI/CalculadoraPRO/CalculadoraPRO/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace CalculadoraPRO.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
