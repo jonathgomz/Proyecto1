40577266199ed4aae73d39681d9a4b1462747e9a
Luis Moreno <luis.moreno16@utp.ac.pa>
2025-11-24 18:55:42 -0500

    Delete Proyecto2 directory

--- Proyecto2/Proyecto2/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Proyecto2.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- Proyecto2/Proyecto2/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Proyecto2.WebApiApplication" Language="C#" %>
