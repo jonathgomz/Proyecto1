338668d7712aa29433611b5f5ba51cd3f2c41316
Jonathan <Jonathan@DESKTOP-UEVN9UU>
2025-11-24 21:21:51 -0500

    movi la cosa

---
+++ Proyecto2/Proyecto2/Models/Calculos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Proyecto2.Models
+{
+    [Table("HistorialCalculos")]
+    public class Calculos
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Operacion { get; set; } 
+        public string Resultado { get; set; }
+        public DateTime FechaHora { get; set; }
+
+        public Calculos() { }
+    }
+}
